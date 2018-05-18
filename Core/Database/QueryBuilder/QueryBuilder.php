65d686c9cc8f866820945d7de7fede46e03dd8a6
Isaac Tunduny <itcyborg@outlook.com>
2018-05-18 09:05:22 +0300

    Integrated functions to Database query builder

--- Core/Database/QueryBuilder/QueryBuilder.php
+++ Core/Database/QueryBuilder/QueryBuilder.php
@@ -24,6 +24,49 @@ class QueryBuilder extends Connection
 
     public static function find($table,$id)
     {
+        return self::$query = sprintf('select * from %s where id= "%s"', $table, $id);
+    }
+
+    public static function one($table, $field, $value, $data)
+    {
+        return self::$query = "SELECT " . $value . " FROM " . $table . " WHERE " . $field . "=" . $data;
+    }
+
+    public static function where($table, $field, $value)
+    {
+        return self::$query = "SELECT * FROM " . $table . " WHERE " . $field . "= '" . $value . "'";
+    }
+
+    public static function delete($table, $field, $value)
+    {
+        return self::$query = "DELETE FROM " . $table . " WHERE " . $field . "=" . $value;
+    }
+
+    public static function deleteAll($table)
+    {
+        return self::$query = "DELETE FROM " . $table;
+    }
+
+    public static function update($table, $fields, $values, $target, $data)
+    {
+        for ($i = 0; $i < count($fields); $i++) {
+            return self::$query = "UPDATE " . $table . " SET " . $fields[$i] . "='" . $values[$i] . "' WHERE " . $target . " = " . $data;
+        }
+    }
+
+    public static function add($table, $fields, $values)
+    {
+        $sql = "INSERT INTO " . $table . "(";
+        for ($i = 0; $i < count($fields); $i++) {
+            $sql .= $fields[$i] . ",";
+        }
+        $sql = (substr($sql, -1) == ',') ? substr($sql, 0, -1) : $sql;
+        $sql .= ") VALUES(";
+        for ($i = 0; $i < count($values); $i++) {
+            $sql .= "'" . $values[$i] . "',";
+        }
+        return self::$query = (substr($sql, -1) == ',') ? substr($sql, 0, -1) : $sql;
+        $sql .= ")";
     }
 
 }
